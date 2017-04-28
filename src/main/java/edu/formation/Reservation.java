d83c4ddf6d7a1150cdc0a54ac3239fc73c1f4ebc
camille <tal.adoum@gmail.com>
2017-04-28 14:50:52 +0200

    resa1

--- src/main/java/edu/formation/Reservation.java
+++ src/main/java/edu/formation/Reservation.java
@@ -1,5 +1,23 @@
 package edu.formation;
 
-public class Reservation {
+import java.sql.Date;
+
+public class Reservation 
+{
+	// id date numero etat
+	private int id;
+	private int numero;
+	private Date date;
+	private EtatReservation etat;
 	
+	//pour incrémenter une nouvelle reservation
+	public static int nbReservation=0;
+	
+	//Instanciation de la classe Reservation sans oublier id pour incrémenter
+	public Reservation(int numero, Date date)
+	{
+		this.id = ++nbReservation;
+		this.numero = numero;
+		this.date = date;
+	}
 }
